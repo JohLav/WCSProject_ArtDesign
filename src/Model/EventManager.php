ed6a91a8ab560622c4f3dd87903725e3c2bcda36
Lervy Myriam <[javamyriam@gmail.com](mailto:javamyriam@gmail.com)>
2023-03-28 12:10:35 +0200

    Formulaires et contro/manag

---
+++ src/Model/EventManager.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Model;
+
+use PDO;
+
+class PlaceManager extends AbstractManager
+{
+    public const TABLE = 'place';
+
+    /**
+     * Insert new item in database
+     */
+    public function insert(array $item): int
+    {
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`kind`,`address`,`city_code`,`city`,`phone`) VALUES (:kind ,:address, :city_code, :city, :phone )");
+        $statement->bindValue('kind', $item['kind'], PDO::PARAM_STR);
+        $statement->bindValue('adress', $item['address'], PDO::PARAM_STR);
+        $statement->bindValue('city_code', $item['city_code'], PDO::PARAM_STR);
+        $statement->bindValue('city', $item['city'], PDO::PARAM_STR);
+        $statement->bindValue('phone', $item['phone'], PDO::PARAM_STR);
+
+
+        $statement->execute();
+        return (int)$this->pdo->lastInsertId();
+    }
+
+    /**
+     * Update item in database
+     */
+    public function update(array $item): bool
+    {
+        $statement = $this->pdo->prepare("UPDATE " . self::TABLE . " SET (`kind`,`address`,`city_code`,`city`,`phone`) = :title WHERE id=:id");
+        $statement->bindValue('id', $item['id'], PDO::PARAM_INT);
+        $statement->bindValue('kind', $item['kind'], PDO::PARAM_STR);
+        $statement->bindValue('adress', $item['address'], PDO::PARAM_STR);
+        $statement->bindValue('kind', $item['city_code'], PDO::PARAM_STR);
+        $statement->bindValue('kind', $item['city'], PDO::PARAM_STR);
+        $statement->bindValue('kind', $item['phone'], PDO::PARAM_STR);
+
+        return $statement->execute();
+
+    }
+}
